e385a7674f7dd026b2b376ceb9868881ede71ce6
aloproy <134494161+aloproy@users.noreply.github.com>
2023-08-15 04:36:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./components/navbar.js"
+import Main from "./components/main.js"
+
+export default function App() {
+    return (
+        <div class="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
